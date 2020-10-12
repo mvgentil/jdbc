3e6a2ed235bfde3392a93fdc92f5865c0b2797d3
Matheus Gentil <matheusvgentil@gmail.com>
2020-10-12 16:32:31 -0300

    seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,11 @@
 package application;
 
 import java.sql.Connection;
+import java.util.Date;
 
 import db.DB;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -14,7 +16,9 @@ public class Program {
 		
 		Department obj = new Department(1, "Books");
 		
-		System.out.println(obj);
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(seller);
 	}
 
 }
