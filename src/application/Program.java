7b3105c2d2b242575341ee967d120f30415853ee
Matheus Gentil <matheusvgentil@gmail.com>
2020-10-12 17:14:51 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,9 +13,6 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Connection conn = DB.getConnection();
-		DB.closeConnection();
-		
 		Department obj = new Department(1, "Books");
 		
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
--- src/db/DB.java
+++ src/db/DB.java
@@ -4,11 +4,13 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
-	
+
 	private static Connection conn = null;
 	
 	public static Connection getConnection() {
@@ -21,7 +23,6 @@ public class DB {
 			catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
-			
 		}
 		return conn;
 	}
@@ -30,16 +31,14 @@ public class DB {
 		if (conn != null) {
 			try {
 				conn.close();
-			}
-			catch (SQLException e) {
+			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
 	}
 	
-	
 	private static Properties loadProperties() {
-		try (FileInputStream fs = new FileInputStream("db.properties")){
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
 			Properties props = new Properties();
 			props.load(fs);
 			return props;
@@ -48,5 +47,24 @@ public class DB {
 			throw new DbException(e.getMessage());
 		}
 	}
+	
+	public static void closeStatement(Statement st) {
+		if (st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
 
+	public static void closeResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
 }
